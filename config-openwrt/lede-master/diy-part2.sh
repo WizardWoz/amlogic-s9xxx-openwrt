423be08d4680c7dc90f54bdbc69000eea1aed878
WizardWoz <61910047+WizardWoz@users.noreply.github.com>
2023-08-01 21:17:40 +0800

    Update diy-part2.sh

--- config-openwrt/lede-master/diy-part2.sh
+++ config-openwrt/lede-master/diy-part2.sh
@@ -60,4 +60,4 @@ rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 增加 luci-app-cpufreq 对 aarch64 的支持，以便在我们的固件中使用（有些修改要谨慎，你必须知道你在做什么）
-sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
+# sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci-app-cpufreq/Makefile
